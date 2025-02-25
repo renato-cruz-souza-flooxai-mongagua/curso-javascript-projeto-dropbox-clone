2ddabce2b31834d792be38c8c946d6c6ed23781f
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-25 10:56:38 -0300

    feat: add file reading logic with error handling

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,14 +8,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/file', (req, res) => {
+  let path = './' + req.query.path;
+
+  if (fs.existsSync(path)) {
+
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).json({
+          error: err
+        });
+      } else {
+        res.status(200).end(data);
+      }
+    })
+
+  } else {
+    res.status(404).json({
+      error: 'File not found.'
+    })
+  }
+})
+
 router.delete('/file', (req, res) => {
-
- 
   let form = new formidable.IncomingForm({
-
     uploadDir: './upload',
     keepExtensions: true
-
   })
 
   form.parse(req, (err, fields, files) => {
@@ -23,50 +42,38 @@ router.delete('/file', (req, res) => {
     let path = './' + fields.path;
 
     if (fs.existsSync(path)) {
-
       fs.unlink(path, err => {
-
         if (err) {
-
           res.status(400).json({
             err
-          });
+          })
         } else {
-
           res.json({
             fields
           })
-
         }
-
-      });
-
+      })
+    } else {
+      res.status(404).json({
+        error: 'File not found.'
+      })
     }
-
+    
   })
-
 })
 
-
-
-router.post('/upload', (req, res)=> {
+router.post('/upload', (req, res) => {
 
   let form = new formidable.IncomingForm({
-
     uploadDir: './upload',
     keepExtensions: true
-
   })
 
   form.parse(req, (err, fields, files) => {
-
     res.json({
       files
     })
-
   })
-
-
 
 })
 
